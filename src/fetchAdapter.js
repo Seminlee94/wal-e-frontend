51c9bc5935dc1cde29878510514863c3439178c1
Se min Lee <mlsml94@gmail.com>
2020-09-04 11:37:37 -0400

    js fetch

--- src/fetchAdapter.js
+++ src/fetchAdapter.js
@@ -1,4 +1,15 @@
 class FetchAdapter {
 
+    constructor(baseUrl){
+        this.baseUrl = baseUrl
+    }
+
+    get(relativeUrl, callback) {
+
+        fetch(`${this.baseUrl}${relativeUrl}`)
+            .then(resp => resp.json())
+            .then(callback)
+
+    }
 
 }
