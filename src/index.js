5b59f4fc065b498b91e2cfcb654dbe16d11ea6e4
Se min Lee <mlsml94@gmail.com>
2020-09-05 12:27:34 -0400

    css

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const ddAisle = document.querySelector(".dropdown-aisle")
     const mainContainers = document.querySelector("#main-containers")
     const sideNav = document.querySelector(".item-side-nav")
+    const blank = document.querySelector("#blank")
 
 
     // const userFetchAdapter = new FetchAdapter("http://localhost:3000/")
@@ -27,22 +28,33 @@ document.addEventListener('DOMContentLoaded', () => {
     // click the items in dropdown list to fetch the category api
 
     ddAisle.addEventListener("click", (e) => {
-        itemCategory = e.target.innerText 
-        //dataset.num ( category ID )
         let categoryId = e.target.dataset.num
+        let itemCategory = e.target.innerText 
+
+        blank.innerHTML = `
+            <h2>${itemCategory}</h2>
+        `
+        blank.style.backgroundColor = "DimGrey";
+        blank.style.height = "50px";
+        // debugger
+
+        //dataset.num ( category ID )
         const newDiv = document.createElement("div")
         newDiv.dataset.num = categoryId
         newDiv.classList.add("item-categories")
         mainContainers.innerHTML = `
-            <h1>${itemCategory}</h1>
+        <h1>${itemCategory}</h1>
         `
         
-        sideNav.style.display = "block"
+        //Side Navigation Bar
+        sideNav.style.display = "block"       
         sideNav.innerHTML = `
-            ${itemCategory}
+        ${itemCategory}
         `
         sideNav.dataset.num = categoryId
+        
         // debugger
+        //
     })
 
 
