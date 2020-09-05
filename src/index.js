3830d2b8e0d47e110583710a2fc7cec01296ef4f
Se min Lee <mlsml94@gmail.com>
2020-09-05 00:02:37 -0400

    frontend

--- src/index.js
+++ src/index.js
@@ -1,10 +1,33 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const navCart = document.querySelector(".nav-cart")
+    const cartContainer = document.querySelector(".cart-container")
+    const mainContainers = document.querySelector("#main-containers")
+    const ddAisle = document.querySelector(".dropdown-aisle")
 
-    const userFetchAdapter = new FetchAdapter("http://localhost:3000/")
+    // const userFetchAdapter = new FetchAdapter("http://localhost:3000/")
 
 
     //Fetch all users
-    const action = users => users.forEach(console.log)
-    userFetchAdapter.get("users", action)
-    
+    // const action = users => users.forEach(console.log)
+    // userFetchAdapter.get("users", action)
+
+
+    // hide & seek with the cart
+    navCart.addEventListener("click", () => {
+        if (cartContainer.style.display === "block") {
+            cartContainer.style.display = "none"
+        } else if (cartContainer.style.display === "none") { 
+          cartContainer.style.display = "block"
+        }
+    });
+
+    ddAisle.addEventListener("click", (e) => {
+        itemCategory = e.target.innerText 
+        mainContainers.innerHTML = `
+            <h1>${itemCategory}</h1>
+        `
+    })
+
+
 })
+
